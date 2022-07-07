79d83cea7aad8f72d3f3d3443ca70964b408ef36
lesenelir <miaomiaobabyzy@gmail.com>
2022-07-07 21:21:19 +0800

    feat: complete  homeright header

--- admin-system/src/components/HomeRight/HomeRight.jsx
+++ admin-system/src/components/HomeRight/HomeRight.jsx
@@ -1,21 +1,47 @@
 import React from 'react'
-import {Breadcrumb, Button, Layout} from "antd"
-import {MenuFoldOutlined, MenuUnfoldOutlined} from "@ant-design/icons"
+import {Breadcrumb, Button, Space, Menu, Dropdown, Layout} from "antd"
+import {MenuFoldOutlined, MenuUnfoldOutlined, DownOutlined} from "@ant-design/icons"
+
+import './HomeRight.css'
+import avatar from '../../assets/images/avatar.jpg'
 
 const {Header, Content, Footer} = Layout
 
 function HomeRight(props) {
 
+  const menu = (
+      <Menu
+          items={[
+            {
+              key: '1',
+              label: ( '修改资料'),
+            },
+            {
+              key: '2',
+              label: ( '退出登录'),
+            }
+          ]}
+      />
+  )
+
   return (
-      <Layout className="site-layout">
-        <Header className="site-layout-background" style={{padding: 0, backgroundColor: '#fff'}}>
+      <Layout className="site-right">
+        <Header className="site-right-header">
           <Button
               style={{marginLeft: '12px', marginBottom: 16}}
               onClick={() => props.setCollapsed(!props.collapsed)}
           >
             {props.collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}
           </Button>
-
+          <Dropdown overlay={menu}>
+            <a href="!#" onClick={(e) => e.preventDefault()}>
+              <Space>
+                <img src={avatar} alt="" className="site-avatar"/>
+                喵宝宝
+                <DownOutlined />
+              </Space>
+            </a>
+          </Dropdown>
         </Header>
 
         <Content style={{margin: '0 16px'}}>
