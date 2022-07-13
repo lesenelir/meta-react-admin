223c007d26a39167496e6c9cb336b1756134b401
lesenelir <miaomiaobabyzy@gmail.com>
2022-07-13 16:28:27 +0800

    add article items

--- koa-server/router/manage/article/index.js
+++ koa-server/router/manage/article/index.js
@@ -4,8 +4,17 @@ const list = require('./list')
 const info = require('./info')
 const edit = require('./edit')
 const deleteArticle = require('./delete')
+const {queryFn} = require("../../../utils/utils")
 
 const router = new Router()
+
+router.get('/', async ctx => {
+  for (let i = 0; i < 100; i++) {
+    let sql = `INSERT INTO article VALUES (null, '你好${i}', '世界${i}', 'lesenelir', '2022-07-13 17:00:00', '内容${i}')`
+    await queryFn(sql)
+  }
+  ctx.body = 1
+})
 
 
 // 文章添加
